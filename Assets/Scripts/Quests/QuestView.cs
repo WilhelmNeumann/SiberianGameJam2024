d91eea75565d461ff1c19829f1c0148ab2834957
aodintsov <aodintsov@bigpoint.net>
2024-11-02 12:24:31 +0100

    Quest view

---
+++ Assets/Scripts/Quests/QuestView.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+namespace Quests {
+    public class QuestView : MonoBehaviour {
+        
+        [SerializeField]
+        private TextMeshProUGUI textDescription;
+        
+        private Quest _quest;
+        
+        public void SetQuest(Quest quest) {
+            _quest = quest;
+            textDescription.text = _quest.ShortDescription;
+        }
+        
+
+    }
+}
