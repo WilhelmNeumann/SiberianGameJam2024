796b37f3dcdb596054efe0af639e6b872377eeba
Aleksandr <shumaherr@gmail.com>
2024-11-03 20:28:52 +0100

    Map regions components

---
+++ Assets/Scripts/Map/LocationType.cs
@@ -0,0 +1,16 @@
+﻿namespace Map
+{
+    public enum LocationType
+    {
+        Swamp,
+        Mill,
+        DwarfMines,
+        Forest,
+        MageTowers,
+        Town,
+        FishingVillage,
+        Farmland,
+        Lake,
+        DevilMountain
+    }
+}
