e73c8562f630ce8d80151602b0cf4355ff2013c8
wilhelm <wilhelm.novitsky@gmail.com>
2024-11-04 10:41:26 +0100

    Panels animations

--- Assets/Scripts/Utils/UIManager.cs
+++ Assets/Scripts/Utils/UIManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 namespace Utils
 {
@@ -13,13 +14,29 @@ namespace Utils
                 return;
             }
             panel.gameObject.SetActive(true);
+            
+            var originalPosition =  panel.transform.position;
+            panel.transform.position = originalPosition + new Vector3(0, -2500, 0);
+            panel.transform.DOJump(originalPosition, 1f, 1, .5f)
+                .OnKill(() => panel.transform.position = originalPosition)
+                .SetAutoKill(true);
+            
             IsPanelOpen = true;
         }
         
         public void ClosePanel(RectTransform panel)
         {
-            panel.gameObject.SetActive(false);
-            IsPanelOpen = false;
+            var originalPosition =  panel.transform.position;
+            var destination = originalPosition + new Vector3(0, -2500, 0);
+            panel.transform.DOJump(destination, 1f, 1, .5f)
+                .OnKill(() =>
+                {
+                    panel.transform.position = originalPosition;
+                    panel.gameObject.SetActive(false);
+                    IsPanelOpen = false;
+                })
+                .SetEase(Ease.OutCubic)
+                .SetAutoKill(true);
         }
         
     }
