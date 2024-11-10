1e20952fdc831f0db847e36a5e453a74093e9a1b
Aleksandr <shumaherr@gmail.com>
2024-11-10 02:09:32 +0100

    Add pagination to the visitor journal

--- Assets/Scripts/Ui/JournalPanelView.cs
+++ Assets/Scripts/Ui/JournalPanelView.cs
@@ -1,32 +1,88 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
 using DefaultNamespace;
 using Npc;
 using Ui;
 using UnityEngine;
-using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace Quests
 {
     public class JournalPanelView : CloseablePanel
     {
         [SerializeField] private VisitorEntryView visitorEntryViewPrefab;
-        [SerializeField] private Transform content;
+        [SerializeField] private Transform leftColumn;
+        [SerializeField] private Transform rightColumn;
+        [SerializeField] private Button nextButton;
+        [SerializeField] private Button previousButton;
+
+        private int currentPage = 0;
+        private const int ItemsPerPage = 4; // 2 элемента на колонку, всего 4 элемента на страницу
+        private List<NpcData> npcLogs;
+        private int totalPages;
 
         private void OnEnable()
         {
-            foreach (Transform child in content)
+            nextButton.onClick.AddListener(NextPage);
+            previousButton.onClick.AddListener(PreviousPage);
+
+            npcLogs = NpcFactory.HeroLogs.ToList();
+            totalPages = Mathf.CeilToInt(npcLogs.Count / (float)ItemsPerPage);
+
+            DisplayPage();
+        }
+
+        private void OnDisable()
+        {
+            nextButton.onClick.RemoveListener(NextPage);
+            previousButton.onClick.RemoveListener(PreviousPage);
+        }
+
+        private void DisplayPage()
+        {
+            // Очищаем предыдущий контент
+            foreach (Transform child in leftColumn)
             {
                 Destroy(child.gameObject);
             }
 
-            NpcFactory.HeroLogs.ToList().ForEach(AddNPC);
+            foreach (Transform child in rightColumn)
+            {
+                Destroy(child.gameObject);
+            }
+
+            // Вычисляем элементы для текущей страницы
+            int startIdx = currentPage * ItemsPerPage;
+            int endIdx = Mathf.Min(startIdx + ItemsPerPage, npcLogs.Count);
+
+            for (int i = startIdx; i < endIdx; i++)
+            {
+                Transform parentColumn = i % 2 == 0 ? leftColumn : rightColumn; // Чередуем колонки
+                var journalView = Instantiate(visitorEntryViewPrefab, Vector2.zero, Quaternion.identity, parentColumn);
+                journalView.SetVisitor(npcLogs[i]);
+            }
+
+            // Обновляем видимость кнопок
+            previousButton.gameObject.SetActive(currentPage > 0);
+            nextButton.gameObject.SetActive(currentPage < totalPages - 1);
         }
 
-        private void AddNPC(NpcData npc)
+        private void NextPage()
         {
-            var journalView = Instantiate(visitorEntryViewPrefab, Vector2.zero, Quaternion.identity, content);
-            journalView.SetVisitor(npc);
+            if (currentPage < totalPages - 1)
+            {
+                currentPage++;
+                DisplayPage();
+            }
+        }
+
+        private void PreviousPage()
+        {
+            if (currentPage > 0)
+            {
+                currentPage--;
+                DisplayPage();
+            }
         }
     }
 }
